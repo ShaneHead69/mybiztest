93e3c9f50a213cc12a9f1be35e041d6d8b3f4196
shanehead69 <shane.head69@hotmail.com.au>
2016-05-13 11:37:03 +0000

    Contracts Controller and defs updated

---
+++ app/controllers/contracts_controller.rb
@@ -0,0 +1,46 @@
+class ContractsController < ApplicationController
+    before_action :require_user
+    before_filter :load_contact
+    
+    def index
+        @contract = @contact.contract.all
+    end
+    
+    def new
+        @contract = @contact.contract.new
+    end
+    
+    def create
+        @contract = @contact.contract.new(params[:contract])
+            if @contract.save
+                redirect_to contracts_path
+            end
+    end
+    
+    def edit
+        @contract = @contact.contract.find(params[:id])
+    end
+    
+    def update
+        @contract = @contact.contract.find(params[:id])
+            if @contract.save
+                redirect_to contract_path
+            end
+    end
+        
+    def show
+        @contract = @contact.contract.find(params[:id])
+    end
+    
+    def destroy
+        @contract = @contact.contract.fnd(params[:id])
+        @contract.destroy
+        redirect_to contracts_path
+    end
+    
+    private
+    def load_contact
+        @contact - Contact.find(params[contact_id])
+    end
+    
+end
